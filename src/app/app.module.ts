88cd0905807a8e430744c3dc1cfbf7c647bf8d1a
LauraHerG <114829844+LauraHerG@users.noreply.github.com>
2023-08-04 19:55:52 -0400

    Added home and navbar components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
